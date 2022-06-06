ceda4d9374994af21934c857b921a30fd8b53458
sanyalu <sanyalu97@gmail.com>
2022-06-06 18:40:57 +0300

    2. HTML to React, creating components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React from "react";
+import './scss/app.scss';
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 
-import './App.css';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort/>
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        <PizzaBlock title='Пепперони' price={350}/>
+                        <PizzaBlock title='Карбонара' price={400}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
